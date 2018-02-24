486762c50a121c54398f7826e382f30505fa4579
TamaraG13 <36139219+TamaraG13@users.noreply.github.com>
2018-02-24 22:53:01 +0100

    Created the Gradebook project

--- Gradebook/src/gradebook/Main.java
+++ Gradebook/src/gradebook/Main.java
@@ -1,41 +1,63 @@
 package gradebook;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main
+
+	{
 
 	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) { 
 	
-		String names[] = new String[5];
-		String surnames[] = new String[5];
-		Double grades[] = new Double[5];
-   
-		names[0] = "Dejan";
-		names[1] = "Tijana";
-		names[2] = "Milos";
-		names[3] = "Irina";
-		names[4] = "Maja";
- 
-		surnames[0] = "Petrovic";
-		surnames[1] = "Mitrovic";
-		surnames[2] = "Simic";
-		surnames[3] = "Lukic";
-		surnames[4] = "Kovacevic";
- 
-		grades[0] = 8.56;
-		grades[1] = 7.96;
-		grades[2] = 9.32;
-		grades[3] = 6.59;
-		grades[4] = 8.87;
- 
-		for(int i = 0; i < 5; i++) {
-		System.out.println(names[i] + " " + surnames[i] + " " + grades[i]);
-     }
-     
-		System.out.println("Now the system calculate the sum");
-		double total = grades[0] + grades[1] + grades[2] + grades[3] + grades[4];
-  
-		System.out.println("Sum of grades is:" + total);
-  
-	  		 
+			System.out.println("1.Student name");
+			String name1 = sc.next();
+			
+			System.out.println("1.Student surname");
+			String surname1 = sc.next();
+			
+			System.out.println("1.Student grade");
+			double grade1 = sc.nextDouble();
+			
+			System.out.println("2.Student name");
+			String name2 = sc.next();
+			
+			System.out.println("2.Student surname");
+			String surname2 = sc.next();
+			
+			System.out.println("2.Student grade");
+			double grade2 = sc.nextDouble();
+			
+			System.out.println("3.Student name");
+			String name3 = sc.next();
+			
+			System.out.println("3.Student surname");
+			String surname3 = sc.next();
+			
+			System.out.println("3.Student grade");
+			double grade3 = sc.nextDouble();
+			
+			System.out.println("4.Student name");
+			String name4 = sc.next();
+			
+			System.out.println("4.Student surname");
+			String surname4 = sc.next();
+			
+			System.out.println("4.Student grade");
+			double grade4 = sc.nextDouble();
+			
+			System.out.println("5.Student name");
+			String name5 = sc.next();
+			
+			System.out.println("5.Student surname");
+			String surname5 = sc.next();
+			
+			System.out.println("5.Student grade");
+			double grade5 = sc.nextDouble();
+			
+			System.out.println("Now system is calculating the sum of grades");
+			double total = grade1 + grade2 + grade3 + grade4 + grade5;
+			
+			System.out.println("Sum of grades is:" + total);
 	}
-
+}
 }
