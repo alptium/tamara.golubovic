22190a3a1591893de0e954e7a109ef39d34efa07
TamaraG13 <36139219+TamaraG13@users.noreply.github.com>
2018-03-17 21:10:52 +0100

    update

--- elite-sports-team-selector/src/elitesportsteamselector/Main.java
+++ elite-sports-team-selector/src/elitesportsteamselector/Main.java
@@ -1,34 +1,86 @@
 package elitesportsteamselector;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		// Basketball team
-		
 		try(Scanner sc = new Scanner(System.in)) {
-
+			
+			System.out.println("Welcome to the application page for our Basketball team!");
+			System.out.println("If you want to join us please follow these simple steps.");
 			System.out.println("In order to apply for this team fill out this form");
-			System.out.println("What is your age?");
-			int yourAge = sc.nextInt();
-		
-			System.out.println("What is your gender?");
-			char yourGender = sc.next().charAt(0);
-		
-			System.out.println("I have experience: true or false?");
-			boolean yourExperience = sc.nextBoolean();
-		
-			System.out.println("What is your expected salary?");
-			double yourSalary = sc.nextDouble();
-		
-			if (yourExperience == true && yourSalary <= 563.33 && yourAge >= 18 && yourGender == 'M') {
-				System.out.println("Congratulation! You are accepted to basketball team!");	
-			} else {
-				System.out.println("We are sorry, your request has been rejected");
-			}
-		
+			
+			runDemoSportsselector(); 
 		}
 	}
+ 			private static void runDemoSportsselector() {
+ 				
+ 				ArrayList<Sportsselector> acceptedList = new ArrayList<>();
+ 				ArrayList<Sportsselector> rejectedList = new ArrayList<>();
+	
+				try(Scanner sc = new Scanner(System.in)) {
+					
+					while(true) {
+					
+					System.out.println("What is your name?");
+					String name = sc.next();
+					
+					System.out.println("What is your surname?");
+					String surname = sc.next();
+					
+					System.out.println("What is your age?");
+					int age = sc.nextInt();
+
+					System.out.println("What is your gender?");
+					char gender = sc.next().charAt(0);
+
+					System.out.println("Do you have experience: true or false?");
+					boolean hasExperience = sc.nextBoolean();
+
+					System.out.println("What is your expected salary?");
+					double salary = sc.nextDouble();
+					
+					Sportsselector sportsselector = new Sportsselector (name, surname, age, gender, hasExperience, salary);
+		
+				if (sportsselector.hasExperience() == true && sportsselector.getSalary() <= 563.33 && age >= 18 && sportsselector.getGender() == 'M') {
+					System.out.println("Congratulation! You are accepted to basketball team!");
+					acceptedList.add(sportsselector);
+				} else {
+					System.out.println("We are sorry, your request has been rejected");
+					rejectedList.add(sportsselector);
+				}
+				
+					System.out.println("Do you want to add new member?");
+					System.out.println("If you want to continue click yes for you want to quit click no");
+					String choice = sc.next();
+					
+					while (choice.equals("yes")) {
+						System.out.println("In order to continue complete next form:");
+						runDemoSportsselector();
+					} if (choice.equals("no")) break;
+					
+					}
+						System.out.println("List of accepted candidates:");
+					
+						int i = 1;
+					
+					for (Sportsselector sportsselector : acceptedList) {
+						System.out.println("" + i + "." + "\t" + sportsselector.getName() + "\t" + sportsselector.getSurname());
+						i++;
+					}
+					
+					System.out.println("List of rejected candidates:");
+					System.out.println();
+
+					int j = 1;
+					
+					for(Sportsselector sportsselector : rejectedList) {
+					System.out.println("" + j + "." + "\t" + sportsselector.getName() + "\t" + sportsselector.getSurname());
+					j++;
+			}	
+		}
+ 	}
 }
