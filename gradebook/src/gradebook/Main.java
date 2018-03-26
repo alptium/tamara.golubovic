f24760fc6c9f2571f50c1cee459294c05746d319
Valentina Cupac <valentina.cupac@optivem.com>
2018-03-26 21:11:30 +0200

    Gradebook - code refactoring (methods)

--- gradebook/src/gradebook/Main.java
+++ gradebook/src/gradebook/Main.java
@@ -1,68 +1,49 @@
 package gradebook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try(Scanner sc = new Scanner(System.in)) { 
-	
-			System.out.println("1.Student name");
-			String name1 = sc.next();
 			
-			System.out.println("1.Student surname");
-			String surname1 = sc.next();
-			
-			System.out.println("1.Student grade");
-			double grade1 = sc.nextDouble();
-			
-			System.out.println("2.Student name");
-			String name2 = sc.next();
-			
-			System.out.println("2.Student surname");
-			String surname2 = sc.next();
-			
-			System.out.println("2.Student grade");
-			double grade2 = sc.nextDouble();
-			
-			System.out.println("3.Student name");
-			String name3 = sc.next();
-			
-			System.out.println("3.Student surname");
-			String surname3 = sc.next();
-			
-			System.out.println("3.Student grade");
-			double grade3 = sc.nextDouble();
-			
-			System.out.println("4.Student name");
-			String name4 = sc.next();
-			
-			System.out.println("4.Student surname");
-			String surname4 = sc.next();
-			
-			System.out.println("4.Student grade");
-			double grade4 = sc.nextDouble();
-			
-			System.out.println("5.Student name");
-			String name5 = sc.next();
-			
-			System.out.println("5.Student surname");
-			String surname5 = sc.next();
-			
-			System.out.println("5.Student grade");
-			double grade5 = sc.nextDouble();
+			Student student1 = readStudent(sc, 1);
+			Student student2 = readStudent(sc, 2);
+			Student student3 = readStudent(sc, 3);
+			Student student4 = readStudent(sc, 4);
+			Student student5 = readStudent(sc, 5);
 			
 			System.out.println("Here is the list of all students and their grades:");
-			System.out.println(name1 + " " + surname1 + " " + grade1);
-			System.out.println(name2 + " " + surname2 + " " + grade2);
-			System.out.println(name3 + " " + surname3 + " " + grade3);
-			System.out.println(name4 + " " + surname4 + " " + grade4);
-			System.out.println(name5 + " " + surname5 + " " + grade5);
+			
+			printStudent(student1);
+			printStudent(student2);
+			printStudent(student3);
+			printStudent(student4);
+			printStudent(student5);
 			
 			System.out.println("Now system is calculating the sum of grades");
-			double total = grade1 + grade2 + grade3 + grade4 + grade5;
+			double total = student1.getGrade() + student2.getGrade() + student3.getGrade() + student4.getGrade() + student5.getGrade();
 			
 			System.out.println("Sum of grades is:" + total);
 		}
+	}
+	
+	private static Student readStudent(Scanner sc, int position) {
+		
+		System.out.println(position + ". Student name");
+		String name = sc.next();
+		
+		System.out.println(position + ". Student surname");
+		String surname = sc.next();
+		
+		System.out.println(position + ". Student grade");
+		double grade = sc.nextDouble();
+		
+		return new Student(name, surname, grade);
+	}
+	
+	private static void printStudent(Student student) {
+		System.out.println(student.getName() + " " + student.getSurname() + " " + student.getGrade());
 	}
 }
